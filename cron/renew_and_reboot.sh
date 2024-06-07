980210324fe7428e45e269148189c17f3222dd86
Andrey <andrey2455@gmail.com>
2024-06-07 13:24:26 +0700

    docker compose v2

--- cron/renew_and_reboot.sh
+++ cron/renew_and_reboot.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker compose -f ../docker-compose.yml exec certbot certbot renew && docker compose -f ../docker-compose.yml exec nginx nginx -s reload
+docker compose -f /root/gornaya-dolina/docker-compose.yml exec certbot certbot renew && \\
+docker compose -f /root/gornaya-dolina/docker-compose.yml exec nginx nginx -s reload
