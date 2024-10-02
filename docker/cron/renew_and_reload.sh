7083a833196b7573699e840537bcdb063313fb75
Andrey <andrey2455@gmail.com>
2024-10-02 15:50:32 +0700

    made updates for docker

--- docker/cron/renew_and_reload.sh
+++ docker/cron/renew_and_reload.sh
@@ -1,3 +1,3 @@
-COMPOSE="docker compose -f /home/www/gornaya-dolina/docker-compose.yml"
+COMPOSE="docker compose -f /home/www/gornaya-dolina/docker/docker-compose.yml"
 
 $COMPOSE exec certbot certbot renew && $COMPOSE exec nginx nginx -s reload
