9954993381af0482ae0359abd96975511f5ffca7
Danil <litan.yt.terr@gmail.com>
2024-10-02 18:14:06 +0700

    remove ScrollSmoother

--- app/utils/initGSAP.tsx
+++ app/utils/initGSAP.tsx
@@ -1,19 +1,19 @@
 "use client";
-import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+// import { useGSAP } from "@gsap/react";
+// import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
 import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
 
 export function InitGSAP() {
-  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  gsap.registerPlugin(ScrollTrigger);
   
-  useGSAP(() => {
-    ScrollSmoother.create({
-      smooth: 1,
-      smoothTouch: 0.1,
-      effects: true,
-    });
-  });
+  // useGSAP(() => {
+  //   ScrollSmoother.create({
+  //     smooth: 1,
+  //     smoothTouch: 0.1,
+  //     effects: true,
+  //   });
+  // });
 
   return <></>;
 }
