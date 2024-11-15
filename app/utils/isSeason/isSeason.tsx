c7074be48c1f210741495e9e1fb807581ba9a5df
Danil <litan.yt.terr@gmail.com>
2024-11-15 17:35:05 +0700

    bugfix: season start date

--- app/utils/isSeason/isSeason.tsx
+++ app/utils/isSeason/isSeason.tsx
@@ -3,7 +3,7 @@ export function getIsSeasonNow() {
   const year = today.getFullYear();
 
   // Даты начала и конца сезона
-  const startOfSeason = new Date(year, 3, 15); // 15 апреля (месяцы начинаются с 0)
+  const startOfSeason = new Date(year, 2, 1); // 1 марта (месяцы начинаются с 0)
   const endOfSeason = new Date(year, 9, 10); // 10 октября
 
   // Проверяем, попадает ли сегодня в сезон
