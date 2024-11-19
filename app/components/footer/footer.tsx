d9b65e7a3fb9306d9d2cc982c0b50ebc41273c56
Danil <litan.yt.terr@gmail.com>
2024-11-19 10:45:50 +0700

    add: link footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import classes from "./footer.module.css";
+import React from 'react';
+import classes from './footer.module.css';
+import Link from 'next/link';
+import clsx from 'clsx';
 
 const Footer = () => {
   return (
-    <footer className={classes.footer}>
-      <div className="container">
+    <div className="container">
+      <footer className={classes.footer}>
         <p className={classes.text}>&copy;&nbsp;2024, Горная долина</p>
-      </div>
-    </footer>
+        <Link
+          href={'https://axonix.ru/'}
+          className={clsx(classes.text, classes.link)}
+        >
+          Разработка сайтов
+        </Link>
+      </footer>
+    </div>
   );
 };
 
